3ee409950b3ae6e3fd3e6da8c4b3000aff54bb3c
cyan3 <cyan3@ncsu.edu>
2020-01-21 21:37:09 -0800

    Updates

---
+++ precache-manifest.a84005076217b4f2e89da03c89f8380b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9894cb90ee0351a27d00c5c44293f1",
+    "url": "/sponsibly/index.html"
+  },
+  {
+    "revision": "bc582c1a48761eda7ccc",
+    "url": "/sponsibly/static/js/0.30134371.chunk.js"
+  },
+  {
+    "revision": "8baa8727df1982ec4620a918911df65a",
+    "url": "/sponsibly/static/js/0.30134371.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc749a947119f3a46671",
+    "url": "/sponsibly/static/js/10.88a0e74d.chunk.js"
+  },
+  {
+    "revision": "e4cf1fade6d6c54c01ec",
+    "url": "/sponsibly/static/js/3.fd626bf0.chunk.js"
+  },
+  {
+    "revision": "56373047c6834c0ac01fa8356cfdf9f4",
+    "url": "/sponsibly/static/js/3.fd626bf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "732e23df3c89ea6d19c6",
+    "url": "/sponsibly/static/js/4.5c1197d2.chunk.js"
+  },
+  {
+    "revision": "4ab225fa188745888c1f",
+    "url": "/sponsibly/static/js/5.a8e19579.chunk.js"
+  },
+  {
+    "revision": "d3af4d191dd7460d53e9",
+    "url": "/sponsibly/static/js/6.ff93cdc4.chunk.js"
+  },
+  {
+    "revision": "561560360ff752335adb",
+    "url": "/sponsibly/static/js/7.fa2ac127.chunk.js"
+  },
+  {
+    "revision": "d1561b0c430c1134e534",
+    "url": "/sponsibly/static/js/8.026551c0.chunk.js"
+  },
+  {
+    "revision": "237ef8d01cbe75302c33",
+    "url": "/sponsibly/static/js/9.4e77209b.chunk.js"
+  },
+  {
+    "revision": "a135950b1dbb17559028",
+    "url": "/sponsibly/static/js/main.20fa3f5a.chunk.js"
+  },
+  {
+    "revision": "4a3a6471c0660ae74158",
+    "url": "/sponsibly/static/js/runtime-main.082365a2.js"
+  },
+  {
+    "revision": "ea8bb7f4b65e7f8b2438d45463270d82",
+    "url": "/sponsibly/static/media/Github.ea8bb7f4.svg"
+  },
+  {
+    "revision": "686f8efa6e3e28e96d1c08399e8d353d",
+    "url": "/sponsibly/static/media/Google.686f8efa.svg"
+  }
+]);
