cd3d6d538c2b004ad3d2586bb4e6ec658cd17071
cyan3 <cyan3@ncsu.edu>
2020-01-21 22:16:57 -0800

    Updates

--- src/routes/LoggingEditRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LoggingForm from '../components/LoggingForm'
-
-export default function LoggingEditRoute(props) {
-  return (
-    <LoggingForm {...props} />
-  )
-}
